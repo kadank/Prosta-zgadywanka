cdd0a289beb16469253286d65d562f1ab3a00ee2
kadank <kadank3@gmail.com>
2022-07-18 15:57:06 +0200

    Import plików do repo

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace zgadula
+{
+    class Program
+    {
+        class zgadula
+        {
+            private int szukana, odp, licznik;
+            public void gra()
+            {
+                Random rnd = new Random();
+                szukana = rnd.Next(1, 20);
+                licznik = 0;
+                do
+                {
+                    Console.WriteLine("zgadnij liczbę z zakresu 1-20");
+                    odp = int.Parse(Console.ReadLine()) ;
+                    licznik++;
+                        if(odp > szukana)
+                    {
+                        Console.WriteLine("Za duża");
+                    }
+                        else if(odp < szukana)
+                    {
+                        Console.WriteLine("Za mała");
+                    }
+                    else
+                    {
+                        Console.WriteLine("oł je zgadłeś");
+                        Console.WriteLine("Liczba prob:");
+                        Console.WriteLine(licznik);
+                    }
+
+                }
+                while (odp != szukana);
+            }
+            
+        }
+       
+        
+
+
+        static void Main(string[] args)
+        {
+            zgadula z = new zgadula();
+            z.gra();
+        }
+    }
+
+}
